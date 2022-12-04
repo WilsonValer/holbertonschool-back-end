eaf9e4c485f64ad7a0fa6d3bbe5ab66afe7d9c48
WilsonValer <nederig21@gmail.com>
2022-12-03 21:03:09 -0500

    0. Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import requests
+import json
+from sys import argv
+
+
+id_employe = int(argv[1])
+name_employe = ""
+number_task = 0
+total_number_task = 0
+list_tasks = []
+url_users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+
+url_tasks = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+
+# print('URL: ', response.url)
+# print('Status code: ', response.status_code)
+# print('HTTP header: ', response.headers)
+
+for user in url_users:
+    if user['id'] == id_employe:
+        name_employe = user["name"]
+        break
+
+for task in url_tasks:
+    if task['userId'] == id_employe:
+        list_tasks.append(task['title'])
+        number_task += 1
+    total_number_task += 1
+
+salida = f'Employee {name_employe} is done with tasks ({number_task}/{total_number_task}):'
+print(salida)
+
+for title in list_tasks:
+    print(f'\t {title}')
