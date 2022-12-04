b9f1c67576d086bb386720214fc6c75761f356d0
WilsonValer <nederig21@gmail.com>
2022-12-04 14:36:59 -0500

    task000

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-"""
-Using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+''' Return information about his/her TODO list progress from an REST API '''
 from requests import get
 import json
 from sys import argv
 
 
-def api_rest():
-    """Write a Python script that, using this REST"""
-    id_employe = int(argv[1])
-    name_employe = ""
+def get_api():
+    ''' Gather data from an API '''
+    id_user = int(argv[1])
+    name_employe = ''
     number_task = 0
     total_number_task = 0
-    list_tasks = []
+    list_task = []
+
     url_users = get('https://jsonplaceholder.typicode.com/users').json()
-
-    url_tasks = get('https://jsonplaceholder.typicode.com/todos').json()
-
     for user in url_users:
-        if user['id'] == id_employe:
-            name_employe = user["name"]
+        if user['id'] == id_user:
+            name_employe = user['name']
             break
 
-    for task in url_tasks:
-        if task['userId'] == id_employe:
+    url_task = get('https://jsonplaceholder.typicode.com/todos').json()
+    for task in url_task:
+        if task['userId'] == id_user:
             if task['completed'] is True:
-                list_tasks.append(task['title'])
+                list_task.append(task['title'])
                 number_task += 1
             total_number_task += 1
 
-    salida = f'Employee {name_employe} is done with\
-          tasks ({number_task}/{total_number_task}):'
+    print('Employee {} is done with tasks({}/{}):'.format(name_employe,
+                                                          number_task,
+                                                          total_number_task))
+    for title in list_task:
+        print('\t {}'.format(title))
 
-    for title in list_tasks:
-        print(f'\t {title}')
 
-
-if __name__ == "__main__":
-    api_rest()
+if __name__ == '__main__':
+    get_api()
